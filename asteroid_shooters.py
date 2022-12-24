3b31bd988a25b93890bad5d10ba5218bb9db54fa
Josiah <griggs@Josiahs-MacBook-Pro.local>
2022-12-24 14:22:41 -0600

    Project Setup

--- asteroid_shooters.py
+++ asteroid_shooters.py
@@ -1,1 +1,23 @@
-print("Hello Workd")
+import pygame, sys
+
+# basic setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Asteroid Shooters')
+clock = pygame.time.Clock()
+
+# game loop:
+while True:
+
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # delta time
+    dt = clock.tick() / 1000
+
+    # draw the frame
+    pygame.display.update()
