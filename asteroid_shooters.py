1520a1ec47d2c12ffd039f6a3720dcefe39dd4d9
Josiah <griggs@Josiahs-MacBook-Pro.local>
2022-12-27 11:41:19 -0600

    Laser Class

--- asteroid_shooters.py
+++ asteroid_shooters.py
@@ -18,7 +18,7 @@ class Ship(pygame.sprite.Sprite):
     def laser_timer(self):
         if not self.can_shoot:
             current_time = pygame.time.get_ticks()
-            if current_time - self.shoot_time > 500:
+            if current_time - self.shoot_time > 400:
                 self.can_shoot = True
     
     def input_position(self): 
@@ -27,9 +27,11 @@ class Ship(pygame.sprite.Sprite):
 
     def shoot(self):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
-            print ('shoot')
             self.can_shoot = False
             self.shoot_time = pygame.time.get_ticks()
+
+            # create new laser object in specified location
+            Laser(self.rect.midtop,laser_group)
 
     def update(self):
         self.laser_timer()
@@ -37,20 +39,28 @@ class Ship(pygame.sprite.Sprite):
         self.input_position()
         
 
-
-
 class Laser(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
-        # parent class
         super().__init__(groups)
-        # image 
         self.image = pygame.image.load('/Users/griggs/Desktop/Dev/Games/AsteroidShooters/graphics/laser.png').convert_alpha()
-        # rect
         self.rect = self.image.get_rect(midbottom = pos)
+       
+        # float-based position
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(0, -1)
+        self.speed = 600
 
     def update(self):
-        self.rect.y -= 10
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x),round(self.pos.y))
         
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self,pos,groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('/Users/griggs/Desktop/Dev/Games/AsteroidShooters/graphics/meteor.png')
+
+        # random float-based position 
+        self.pos = pygame.math.Vector2([math.rand,])
 
 # basic setup
 pygame.init()
@@ -65,11 +75,14 @@ background_surface = pygame.image.load('/Users/griggs/Desktop/Dev/Games/Asteroid
 # sprite groups
 spaceship_group = pygame.sprite.Group()
 laser_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
 
 # sprite creation
 ship = Ship(spaceship_group)
-laser = Laser((100,300),laser_group)
 
+# timer
+meteor_timer = pygame.event.custom_type()
+pygame.time.set_timer(meteor_timer,400)
 # game loop:
 while True:
 
@@ -78,6 +91,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == meteor_timer
+            Meteor(pos, group)
 
     # delta time
     dt = clock.tick() / 1000
