93610d6d41e37505815a04f3de877cda277e83d7
Josiah <griggs@Josiahs-MacBook-Pro.local>
2022-12-28 14:57:02 -0600

    Working before collisions

--- asteroid_shooters.py
+++ asteroid_shooters.py
@@ -61,17 +61,33 @@ class Meteor(pygame.sprite.Sprite):
 
         # basic setup 
         super().__init__(groups)
-        self.image = pygame.image.load('/Users/griggs/Desktop/Dev/Games/AsteroidShooters/graphics/meteor.png')
-        self.rect = self.image.get_rect(center = pos) 
-        
+
+        # raondomize meteor size
+        meteor_surf = pygame.image.load('/Users/griggs/Desktop/Dev/Games/AsteroidShooters/graphics/meteor.png').convert_alpha()
+        meteor_size = pygame.math.Vector2(meteor_surf.get_size()) * uniform(0.5,1.5)
+        self.scaled_surf = pygame.transform.scale(meteor_surf, meteor_size)
+        self.image = self.scaled_surf
+        self.rect = self.image.get_rect(center = pos)
+
         # random float-based position 
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2(uniform(-0.5,0.5),1) # (x, y)
         self.speed = randint(400,600)
 
+        # rotation logic
+        self.rotation = 0
+        self.rotation_speed = randint(20,50)
+    
+    def rotate(self):
+        self.rotation += self.rotation_speed * dt
+        rotated_surf = pygame.transform.rotozoom(self.scaled_surf, self.rotation, 1)
+        self.image = rotated_surf
+        self.rect = self.image.get_rect(center = self.rect.center) # create new rect at the same position
+
     def update(self):
         self.pos += self.direction * self.speed * dt
         self.rect.topleft = (round(self.pos.x),round(self.pos.y))
+        self.rotate()
 
 # score class
 
